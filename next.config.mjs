d3db1f3dae8df352bae10fd89898fedbedb80bb6
Sebastian Guerrero <sebasaga8@gmail.com>
2024-07-16 19:53:21 -0400

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,57 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pics.filmaffinity.com',
+        port: '',
+        pathname: '/aOye_Arnold_Serie_de_TV-451908137-large.jpg',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/originals/64/18/8f/64188faa180afde4d36e69cf5636d4e2.jpg',
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/images/M/MV5BMzYyNDU3ZTctYTQwYi00NjVmLTgwN2QtYmFmMTY5MjI0NDdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_FMjpg_UX1000_.jpg',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wallpapers.com',
+        port: '',
+        pathname: '/images/hd/tmnt-1920-x-1080-wallpaper-bfheoo5r7l5m3qcs.jpg',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pics.filmaffinity.com',
+        port: '',
+        pathname: '/samurai_jack_the_premiere_movie-766900773-large.jpg',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.adultswim.com',
+        port: '',
+        pathname: 'misc/samurai-jack-backgrounds/media/SAMURAI_JACK_1920X1080_Skulls.png',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.justwatch.com',
+        port: '',
+        pathname: '/poster/300678811/s718/fenomenoide.jpg',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/originals/fb/1a/97/fb1a97b949c12a30269f813ab996e2da.jpg',
+      }
+    ],
+  },
   async headers() {
     return [
       {
@@ -11,7 +63,7 @@ const nextConfig = {
           },
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://kyber-swart.vercel.app/', // Cambia esto por el dominio que necesites permitir
+            value: '*', // Cambia esto por el dominio que necesites permitir
           },
           {
             key: 'Access-Control-Allow-Methods',
@@ -26,59 +78,5 @@ const nextConfig = {
     ];
   },
 };
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'pics.filmaffinity.com',
-            port: '',
-            pathname: '/aOye_Arnold_Serie_de_TV-451908137-large.jpg',
-          },
-          {
-            protocol: 'https',
-            hostname: 'i.pinimg.com',
-            port: '',
-            pathname: '/originals/64/18/8f/64188faa180afde4d36e69cf5636d4e2.jpg',
-          },
-          {
-            protocol: 'https',
-            hostname: 'm.media-amazon.com',
-            port: '',
-            pathname: '/images/M/MV5BMzYyNDU3ZTctYTQwYi00NjVmLTgwN2QtYmFmMTY5MjI0NDdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_FMjpg_UX1000_.jpg',
-          },
-          {
-            protocol: 'https',
-            hostname: 'wallpapers.com',
-            port: '',
-            pathname: '/images/hd/tmnt-1920-x-1080-wallpaper-bfheoo5r7l5m3qcs.jpg',
-          },
-          {
-            protocol: 'https',
-            hostname: 'pics.filmaffinity.com',
-            port: '',
-            pathname: '/samurai_jack_the_premiere_movie-766900773-large.jpg',
-          },
-          {
-            protocol: 'https',
-            hostname: 'www.adultswim.com',
-            port: '',
-            pathname: 'misc/samurai-jack-backgrounds/media/SAMURAI_JACK_1920X1080_Skulls.png',
-          },
-          {
-            protocol: 'https',
-            hostname: 'images.justwatch.com',
-            port: '',
-            pathname: '/poster/300678811/s718/fenomenoide.jpg',
-          },
-          {
-            protocol: 'https',
-            hostname: 'i.pinimg.com',
-            port: '',
-            pathname: '/originals/fb/1a/97/fb1a97b949c12a30269f813ab996e2da.jpg',
-          }
-        ],
-      },
-};
-
 
 export default nextConfig;
