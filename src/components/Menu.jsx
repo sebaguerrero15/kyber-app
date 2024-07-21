b07745508f304e11816db91d4e1d8d1d6699e141
Sebastian Guerrero <sebasaga8@gmail.com>
2024-07-21 19:11:59 -0400

    acomodar imagenes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,13 +9,13 @@ const Menu = () => {
         <div className='container mx-auto'>
             <div className='grid grid-cols-1 justify-center items-center sm:flex gap-4 m-2 font-bold text-xl'>
             <Link href="/caricatura" className='text-center'>
-                <button className='w-52 h-32 text-center bg-slate-500 border-none uppercase text-white rounded-lg'>
+                <button className='w-52 h-32 text-center bg-[#A91D3A] border-transparent uppercase text-white rounded-lg hover:border-4 hover:border-white hover:bg-black transition-all duration-100'>
                     Series Animación
                 </button>
             </Link>
 
                 <Link href="/peliculas" className='text-center'>
-                <button className="w-52 h-32 bg-[url('https://pa1.aminoapps.com/6694/c4fa8ff034986d3f21c219fca3d96e9fea9a2046_00.gif')] border-transparent uppercase text-white rounded-lg hover:border-4 hover:border-white transition-all duration-75">
+                <button className="w-52 h-32 text-center bg-[#0F3D3E] border-transparent uppercase text-white rounded-lg hover:border-4 hover:border-white hover:bg-black transition-all duration-100">
                     Películas Animación
                 </button>
                 </Link>
